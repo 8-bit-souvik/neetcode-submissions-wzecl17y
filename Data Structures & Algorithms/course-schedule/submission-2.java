cb968db4f9866c5445292c8be3df957b9429eaae
Souvik Mandal <souvikmandal210@gmail.com>
2026-04-26 10:47:14 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.java
@@ -0,0 +1,70 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, Integer> indegree = new HashMap<>();
+        Map<Integer, List<Integer>> pointer = new HashMap<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int[] pre : prerequisites){
+            if(indegree.containsKey(pre[0])){
+                indegree.put(pre[0], indegree.get(pre[0])+1);
+            }else{
+                indegree.put(pre[0], 0);
+                indegree.put(pre[0], indegree.get(pre[0])+1);
+            }
+            if(!indegree.containsKey(pre[1])){
+                indegree.put(pre[1], 0);
+            }
+            if(pointer.containsKey(pre[1])){
+                pointer.get(pre[1]).add(pre[0]);
+            }else{
+                pointer.put(pre[1], new ArrayList<>());
+                pointer.get(pre[1]).add(pre[0]);
+            }
+        }
+
+        indegree.forEach((key, value)-> {
+            if(value == 0){
+                q.offer(key);
+            }
+        });
+
+        // System.out.println(pointer);
+        // System.out.println(indegree);
+
+        while(!q.isEmpty()){
+            Integer opt = q.peek();
+            if(!pointer.containsKey(opt)){
+                q.poll();
+                continue;
+            }
+            List<Integer> nodeList = pointer.get(opt);
+            // System.out.println(opt+": "+nodeList);
+            for(Integer node : nodeList){
+                indegree.put(node, indegree.get(node)-1);
+                // System.out.println(node+" "+indegree.get(node));
+                if(indegree.get(node) == 0){
+                    q.offer(node);
+                    indegree.remove(node);
+                }
+            }
+            // System.out.println("----");
+            q.poll();
+        }
+
+        // System.out.println(indegree);
+        int size = indegree.size();
+        Set<Integer> keys = indegree.keySet(); 
+        for(Integer key: keys){
+            if(indegree.get(key)==0){
+                size--;
+            }
+        }
+
+        if(size > 0){
+            return false;
+        }else{
+            return true
+            ;
+        }
+    }
+}
